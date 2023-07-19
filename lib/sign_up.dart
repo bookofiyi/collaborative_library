957ab954dd277f5cb0f2dd0735f05de7c80dfc57
Iyiola <iyiolaeadedeji@gmail.com>
2023-07-19 17:21:43 +0100

    More updates to sign up page

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,5 +1,7 @@
 import 'package:collab_library/logic/colors.dart';
+import 'package:collab_library/logic/font_family.dart';
 import 'package:collab_library/logic/size_config.dart';
+import 'package:collab_library/widget/customWidgets.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gap/gap.dart';
@@ -13,6 +15,9 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final fNameController = TextEditingController();
+  final lNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
@@ -41,6 +46,124 @@ class _SignUpState extends State<SignUp> {
                     ),
                   ),
                 ),
+                const SizedBox(
+                  height: 30,
+                ),
+                customTitleText(
+                  'User Registration',
+                  spacing: 0,
+                  size: 16,
+                  textAlign: TextAlign.center,
+                  colors: AppColor.normaltextColor,
+                ),
+                customDescriptionText(
+                  'Create an account to continue',
+                  fontSize: 12,
+                  textAlign: TextAlign.center,
+                  colors: AppColor.normaltextColor,
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Column(
+                  children: [
+                    TextFormField(
+                      cursorColor: AppColor.primaryColor,
+                      controller: fNameController,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        fillColor: Colors.grey[500]!.withOpacity(0.2),
+                        filled: true,
+                        focusColor: AppColor.primaryColor,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                              color: AppColor.primaryColor, width: 15),
+                        ),
+                        labelText: 'First Name',
+                        labelStyle: Theme.of(context)
+                            .textTheme
+                            .headlineMedium!
+                            .copyWith(
+                                color: AppColor.inputtextColor.withOpacity(0.4),
+                                fontFamily: FontFamily.sfRegular,
+                                fontSize: 14),
+                        hintText: 'Enter first name',
+                        hintStyle: Theme.of(context)
+                            .textTheme
+                            .headlineMedium!
+                            .copyWith(
+                                color: AppColor.inputtextColor.withOpacity(0.4),
+                                fontFamily: FontFamily.sfRegular,
+                                fontSize: 14),
+                      ),
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'Enter your first name';
+                        }
+                        return null;
+                      },
+                    ),
+                    const Gap(15),
+                    TextFormField(
+                      cursorColor: AppColor.primaryColor,
+                      controller: lNameController,
+                      keyboardType: TextInputType.name,
+                      textInputAction: TextInputAction.done,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        fillColor: Colors.grey[500]!.withOpacity(0.2),
+                        filled: true,
+                        focusColor: AppColor.primaryColor,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(
+                              color: AppColor.primaryColor, width: 15),
+                        ),
+                        labelText: 'Last Name',
+                        labelStyle: Theme.of(context)
+                            .textTheme
+                            .headlineMedium!
+                            .copyWith(
+                                color: AppColor.inputtextColor.withOpacity(0.4),
+                                fontFamily: FontFamily.sfRegular,
+                                fontSize: 14),
+                        hintText: 'Enter last name',
+                        hintStyle: Theme.of(context)
+                            .textTheme
+                            .headlineMedium!
+                            .copyWith(
+                                color: AppColor.inputtextColor.withOpacity(0.4),
+                                fontFamily: FontFamily.sfRegular,
+                                fontSize: 14),
+                      ),
+                      validator: (String? value) {
+                        if (value!.isEmpty) {
+                          return 'Enter your last name';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                )
               ],
             ),
           ),
