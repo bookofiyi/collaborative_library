ef321d853a6b2df15a8cc05f213309c7fa2daa99
Iyiola <iyiolaeadedeji@gmail.com>
2023-07-27 07:33:03 +0100

    lamba!!

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -1,13 +1,17 @@
 import 'package:collab_library/logic/colors.dart';
 import 'package:collab_library/logic/font_family.dart';
+import 'package:collab_library/models/course_resource.dart';
+import 'package:collab_library/repositories/course_repository.dart';
 import 'package:collab_library/screens/all_courses.dart';
 import 'package:collab_library/screens/all_resources.dart';
+import 'package:collab_library/screens/resource_details.dart';
 // import 'package:collab_library/screens/resource_details.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:collab_library/screens/search_result.dart';
 import 'package:collab_library/widget/customWidgets.dart';
 import 'package:flutter/material.dart';
 import 'package:image_card/image_card.dart';
+import 'package:intl/intl.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage(
@@ -23,6 +27,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final user = FirebaseAuth.instance.currentUser;
+  final courseRepository = CourseRepository();
   final TextEditingController _searchFieldController = TextEditingController();
   bool _showClearIcon = false;
 
@@ -488,52 +493,120 @@ class _MainPageState extends State<MainPage> {
                     // ),
 
                     SizedBox(
-                      height: 190,
-                      child: GridView.count(
-                        scrollDirection: Axis.horizontal,
-                        crossAxisCount: 1,
-                        children: List.generate(6, (index) {
-                          return GestureDetector(
-                            onTap: () {
-                              // Navigator.push(
-                              //     context,
-                              //     MaterialPageRoute(
-                              //         builder: (context) =>
-                              //             const ResourceDetailsPage()));
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.only(right: 16.0),
-                              child: FillImageCard(
-                                imageProvider: const AssetImage(
-                                    'assets/images/docPreview.png'),
-                                width: MediaQuery.of(context).size.width * 2.5,
-                                heightImage: 100,
-                                // height: 300,
-                                tags: [customTag(courseName: 'CSC201')],
-                                title: const Text(
-                                  'Intro to CSC201',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                description: const Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      'Uploaded by Someone',
-                                      style: TextStyle(fontSize: 10),
+                      height: 194,
+                      child: FutureBuilder<List<CourseResource>>(
+                        future: courseRepository.getCourseResources('CPE508'),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+
+                          final courseResources = snapshot.data ?? [];
+
+                          if (courseResources.isEmpty) {
+                            return const Center(
+                              child: Text(
+                                  'Be the first to upload a resource to this course'),
+                            );
+                          }
+                          return GridView.count(
+                            crossAxisCount: 1,
+                            scrollDirection: Axis.horizontal,
+                            children:
+                                List.generate(courseResources.length, (index) {
+                              final courseResource = courseResources[index];
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ResourceDetailsPage(
+                                                courseResource: courseResource,
+                                              )));
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: FillImageCard(
+                                    imageProvider: const AssetImage(
+                                        'assets/images/docPreview.png'),
+                                    width:
+                                        MediaQuery.of(context).size.width * 2.5,
+                                    heightImage: 100,
+                                    // height: 300,
+                                    tags: [customTag(courseName: 'CPE508')],
+                                    title: Text(courseResource.title),
+                                    description: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Uploaded by ${courseResource.uploadedBy.isEmpty ? 'Someone' : courseResource.uploadedBy}',
+                                          style: const TextStyle(fontSize: 10),
+                                        ),
+                                        Text(
+                                          DateFormat('dd MMMM yyyy').format(
+                                              courseResource.dateUploaded),
+                                          style: const TextStyle(fontSize: 10),
+                                        ),
+                                      ],
                                     ),
-                                    Text(
-                                      '12th July 2023',
-                                      style: TextStyle(fontSize: 10),
-                                    ),
-                                  ],
+                                    color: const Color(0xFFF0F4FD),
+                                  ),
                                 ),
-                                color: const Color(0xFFF0F4FD),
-                              ),
-                            ),
+                              );
+                            }),
                           );
-                        }),
+                        },
                       ),
+                      // child: GridView.count(
+                      //   scrollDirection: Axis.horizontal,
+                      //   crossAxisCount: 1,
+                      //   children: List.generate(6, (index) {
+                      //     return GestureDetector(
+                      //       onTap: () {
+                      //         // Navigator.push(
+                      //         //     context,
+                      //         //     MaterialPageRoute(
+                      //         //         builder: (context) =>
+                      //         //             const ResourceDetailsPage()));
+                      //       },
+                      //       child: Padding(
+                      //         padding: const EdgeInsets.only(right: 16.0),
+                      //         child: FillImageCard(
+                      //           imageProvider: const AssetImage(
+                      //               'assets/images/docPreview.png'),
+                      //           width: MediaQuery.of(context).size.width * 2.5,
+                      //           heightImage: 100,
+                      //           // height: 300,
+                      //           tags: [customTag(courseName: 'CSC201')],
+                      //           title: const Text(
+                      //             'Intro to CSC201',
+                      //             style: TextStyle(fontWeight: FontWeight.bold),
+                      //           ),
+                      //           description: const Row(
+                      //             mainAxisAlignment:
+                      //                 MainAxisAlignment.spaceBetween,
+                      //             children: [
+                      //               Text(
+                      //                 'Uploaded by Someone',
+                      //                 style: TextStyle(fontSize: 10),
+                      //               ),
+                      //               Text(
+                      //                 '12th July 2023',
+                      //                 style: TextStyle(fontSize: 10),
+                      //               ),
+                      //             ],
+                      //           ),
+                      //           color: const Color(0xFFF0F4FD),
+                      //         ),
+                      //       ),
+                      //     );
+                      //   }),
+                      // ),
                     ),
                     // Row(
                     //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
